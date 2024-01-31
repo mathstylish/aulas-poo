a9fdd75e6b477ac0d4463e53e17fc5288ff9bdfc
Matheus <matheus.hpp135@gmail.com>
2024-01-30 22:52:57 -0300

    feat: 🎸 introdução: definição de POO, classe e objeto

---
+++ src/domain/Car.java
@@ -0,0 +1,17 @@
+package domain;
+
+/**
+ * Representa a abstração de um carro
+ * 
+ * @param name nome do carro
+ * @param model modelo do carro
+ * @param year ano de lançamento
+ * 
+ * @return Um objeto do tipo carro com nome, modelo e ano de lançamento
+ */
+public class Car {
+    // Atributos (características de um carro)
+    public String name;
+    public String model;
+    public int year;
+}
